838e9830b4f9059174832d4b47a4ef2cb11b0327
Ryan Skinner <ryanskinner@gmail.com>
2024-01-30 12:05:55 -0500

    let's try live

--- api/random.js
+++ api/random.js
@@ -107,6 +107,9 @@ const createPost = async (petDetails) => {
             }
         }
 
+        const greetingPhrases = ["Meet", "Say hello to", "Introducing", "Meet your new friend"];
+        const randomGreeting = greetingPhrases[Math.floor(Math.random() * greetingPhrases.length)];
+
         let breedStr = '';
         if (petDetails.breeds.primary) {
             breedStr = petDetails.breeds.primary;
@@ -122,7 +125,7 @@ const createPost = async (petDetails) => {
         }
 
         const formattedName = petDetails.name.trim().replace(/\s+,/, ',');
-        const postText = `Meet ${formattedName}, located in ${petDetails.contact.address.city}, ${petDetails.contact.address.state}.\n\nLearn more: ${petDetails.url}`;
+        const postText = `${randomGreeting} ${formattedName}, located in ${petDetails.contact.address.city}, ${petDetails.contact.address.state}.\n\nLearn more: ${petDetails.url}`;
 
         const rt = new RichText({ text: postText });
         await rt.detectFacets(agent);
