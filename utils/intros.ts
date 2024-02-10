db0b1f0ea76d2b8d7cb761191b7583835504f5f3
Ryan Skinner <ryanskinner@gmail.com>
2024-02-10 17:41:17 -0500

    adding more intros

--- utils/intros.ts
+++ utils/intros.ts
@@ -10,6 +10,16 @@ export function getRandomIntro(): string {
     'A bundle of joy named',
     'Could this be your new cuddle buddy?',
     'Find a place in your heart for',
+    'Embrace the adventure of friendship with',
+    'Your next loyal friend could be',
+    'Imagine the paw-sibilities with',
+    'Welcoming a new friend is a walk in the park with',
+    'Leap into a lifetime of love with',
+    'Let your heart be stolen by',
+    'Paws what you\'re doing and meet',
+    'Every journey begins with a single paw: meet',
+    'Bringing love and wags into your life:',
+    'Share your life with a furry soul named',
   ]
   return intros[Math.floor(Math.random() * intros.length)]
 }
